699b50e2bf1bbb0c9eff92a7d659df4a5bb57a3e
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-08-23 10:40:03 +0530

    Create api.tsx

---
+++ api.tsx
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance } from 'axios';
+
+const BASE_URL = "http://localhost:3000/api/v1/";
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
